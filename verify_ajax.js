b5f52a1a8d417d5fb2120c51c9174feddd39ca59
nazirulhziq <61816543+nazirulhziq@users.noreply.github.com>
2022-09-06 23:11:25 +0800

    Update verify_ajax.js

---
+++ verify_ajax.js
@@ -0,0 +1,40 @@
+function postValue(){
+
+    var username = $('#username').val();
+
+
+    if(!username)
+    {   
+              $('#message').html("Key in username and click submit");
+                message.style.color = "#5fbb81";
+      
+    }
+    else
+    {   
+    
+        $.post('info.php',{username:username},
+        function(output){
+    
+            $('#message').html(output);
+            
+            
+            if(output =="true"){
+                $('#message').html("<b>Verified</b>");
+                  
+                message.style.color = "green";
+            }
+            else 
+            {
+                $('#message').html("<b>Error</b>");
+
+            
+                message.style.color = "red";
+            }
+    
+        });
+        
+        return true;
+    }
+  
+
+}
